ac0d9f7e5692bcde92af9c6a1868ff52532a803d
justmedev22 <skxllzone@gmail.com>
2026-05-03 12:31:11 -0500

    avance de mi parte promedio compose - clase 3

--- Tercera clase/appbasiccomponents/app/src/main/java/pe/edu/idat/appbasiccomponents/PromedioScreen.kt
+++ Tercera clase/appbasiccomponents/app/src/main/java/pe/edu/idat/appbasiccomponents/PromedioScreen.kt
@@ -1,5 +1,6 @@
 package pe.edu.idat.appbasiccomponents
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -25,6 +27,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import pe.edu.idat.appbasiccomponents.ui.theme.AppbasiccomponentsTheme
@@ -46,7 +49,7 @@ fun GreetingPreview() {
 fun PromedioScreen() {
     Box(modifier = Modifier
         .fillMaxSize()
-        .padding(top = 55.dp))
+        .padding(top = 55.dp)) {
 
     /*
         var ec1 by rememberSaveable() {
@@ -68,10 +71,21 @@ fun PromedioScreen() {
         mutableStateOf("")
     }
 
-    // se crea la variable resultado
+
+
+
+        // se crea la variable resultado
     var resultado by rememberSaveable {
         mutableStateOf("")
     }
+
+        val colorTexto = if (resultado.contains("DESAPROBADO")) {
+            Color.Red
+        } else {
+            Color.Green
+        }
+
+
     Column {
             Text(text = "PROMEDIO DE NOTAS IDAT",
                 textAlign = TextAlign.Center,
@@ -88,62 +102,90 @@ fun PromedioScreen() {
             OutlinedTextField(
                 value = ec1,
                 onValueChange = { ec1 = it },
-                modifier = Modifier.fillMaxWidth(),
-                label = { Text(text = "Ingrese EC 1") },
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp)
+                    .fillMaxWidth(),
+                    //.border(border = Color.Black),
+                    //(color = Color.Black),
+                label = { Text(text = "Ingrese EC 1",
+                    style = TextStyle(fontSize = 15.sp,
+                        color = Color.Black) ) },
                 maxLines = 1,
                 singleLine = true,
                 keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number
-                ))
+                    keyboardType = KeyboardType.Number),
+                colors = coloresCampoTexto(colorNota(ec1))
+            )
             mySpace(16)
             OutlinedTextField(
             value = ec2,
             onValueChange = { ec2 = it },
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "Ingrese EC 2") },
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            label = { Text(text = "Ingrese EC 2",
+                style = TextStyle(fontSize = 15.sp,
+                    color = Color.Black)) },
             maxLines = 1,
             singleLine = true,
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            ))
+                keyboardType = KeyboardType.Number),
+                colors = coloresCampoTexto(colorNota(ec2))
+            )
             mySpace(16)
             OutlinedTextField(
             value = ec3,
             onValueChange = { ec3 = it },
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "Ingrese EC 3") },
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            label = { Text(text = "Ingrese EC 3",
+                style = TextStyle(fontSize = 15.sp,
+                    color = Color.Black)) },
             maxLines = 1,
             singleLine = true,
+
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            ))
+                keyboardType = KeyboardType.Number),
+                colors = coloresCampoTexto(colorNota(ec3))
+                )
             mySpace(16)
             OutlinedTextField(
             value = ef,
             onValueChange = { ef = it },
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "Ingrese EF") },
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            label = { Text(text = "Ingrese EF",
+                style = TextStyle(fontSize = 15.sp,
+                    color = Color.Black)) },
             maxLines = 1,
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number
-                )
+                ),  // parte para el color interno de los textos
+                colors = coloresCampoTexto(colorNota(ef))
             )
             mySpace(16)
             Button(onClick = {
                 resultado = calcularPromedio(ec1.toInt(), ec2.toInt(), ec3.toInt(), ef.toInt())
             },
-                modifier = Modifier.fillMaxWidth()) {
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp)
+                    .fillMaxWidth()) {
                 Text(text = "CALCULAR")
             }
             mySpace(16)
             Text(text = resultado,
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp),
                 style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp,
-                    color = Color.DarkGray))
+                    color = colorTexto)
+
+            )
         }
     }
 
-
+}
 @Composable
 fun mySpace(espacio: Int) {
     Spacer(modifier = Modifier.size(espacio.dp))
@@ -157,8 +199,31 @@ fun calcularPromedio(ec1: Int, ec2: Int, ec3: Int, ef: Int): String {
     if(promedio > 12.5) {
         resultado = "APROBADO"
     }
+
     return "Su promedio es $promedio usted se encuentra $resultado"
 }
 
 
+// codigo para el texto claro
+@Composable
+fun coloresCampoTexto(colorTexto: Color) = OutlinedTextFieldDefaults.colors(
+    focusedTextColor = colorTexto,
+    unfocusedTextColor = colorTexto,
+    cursorColor = colorTexto,
+    focusedBorderColor = Color.DarkGray,
+    unfocusedBorderColor = Color.Black,
+    focusedLabelColor = Color.Black,
+    unfocusedLabelColor = Color.Black
+)
 
+// funcion para cambiar de color los numeros ingresados
+@Composable
+fun colorNota(nota: String): Color {
+    val numero = nota.toIntOrNull()
+
+    return when {
+        numero == null -> Color.Black
+        numero >= 13 -> Color.Green
+        else -> Color.Red
+    }
+}
